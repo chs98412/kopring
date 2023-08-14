5c342668272f615a2377dc45fcb1e64484d85e94
chs98412 <chs98412@gmail.com>
2023-08-14 17:52:14 +0900

    post 해보기, dto 만들어보기

--- src/main/kotlin/com/kopring/controller/HelloController.kt
+++ src/main/kotlin/com/kopring/controller/HelloController.kt
@@ -1,10 +1,10 @@
 
 package com.kopring.controller
 
+import com.kopring.dto.HelloDTO
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
@@ -23,6 +23,17 @@ class HelloController {
         return ResponseEntity
                 .ok()
                 .body("Hello ResponseEntity")
+    }
+
+    @PostMapping("/hellopost")
+    fun helloPost(@RequestBody request: HelloDTO): ResponseEntity<Any> {
+        println(request)
+        println(request.id)
+        println(request.name)
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(request)
     }
 
 }
---
+++ src/main/kotlin/com/kopring/dto/HelloDTO.kt
@@ -0,0 +1,6 @@
+package com.kopring.dto
+
+data class HelloDTO(
+        val id: Long? = null,
+        val name: String
+)
